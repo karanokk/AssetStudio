dcc6cc3c231549245c0b246348a0a5b2e86cb929
karanokk <karanokk@icloud.com>
2020-09-03 10:48:19 +0800

    feat: add folder export cli

---
+++ FGOAssetHelper/AssetItem.cs
@@ -0,0 +1,28 @@
+﻿using AssetStudio;
+
+namespace FGOAssetHelper
+{
+    internal class AssetItem
+    {
+        public Object Asset;
+        public SerializedFile SourceFile;
+        public string Container = string.Empty;
+        public string TypeString;
+        public long m_PathID;
+        public long FullSize;
+        public ClassIDType Type;
+        public string InfoText;
+        public string UniqueID;
+        public string Text = string.Empty;
+
+        public AssetItem(Object asset)
+        {
+            Asset = asset;
+            SourceFile = asset.assetsFile;
+            Type = asset.type;
+            TypeString = Type.ToString();
+            m_PathID = asset.m_PathID;
+            FullSize = asset.byteSize;
+        }
+    }
+}
---
+++ FGOAssetHelper/Program.cs
@@ -0,0 +1,163 @@
+﻿using AssetStudio;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using static FGOAssetHelper.Exporter;
+using Object = AssetStudio.Object;
+
+namespace FGOAssetHelper
+{
+    class Program
+    {
+        public static AssetsManager assetsManager = new AssetsManager();
+        public static List<AssetItem> exportableAssets = new List<AssetItem>();
+        static void Main(string[] args)
+        {
+            System.Diagnostics.Debug.Assert(args.Length == 2);
+            assetsManager.LoadFolder(args[0]);
+            BuildAssetData();
+            ExportAssets(args[1], exportableAssets);
+        }
+
+        public static void BuildAssetData()
+        {
+            string productName = null;
+            var objectCount = assetsManager.assetsFileList.Sum(x => x.Objects.Count);
+            var objectAssetItemDic = new Dictionary<Object, AssetItem>(objectCount);
+            var containers = new List<(PPtr<Object>, string)>();
+            int i = 0;
+            Progress.Reset();
+            foreach (var assetsFile in assetsManager.assetsFileList)
+            {
+                foreach (var asset in assetsFile.Objects)
+                {
+                    var assetItem = new AssetItem(asset);
+                    objectAssetItemDic.Add(asset, assetItem);
+                    assetItem.UniqueID = " #" + i;
+                    var exportable = false;
+                    switch (asset)
+                    {
+                        case GameObject m_GameObject:
+                            assetItem.Text = m_GameObject.m_Name;
+                            break;
+                        case Texture2D m_Texture2D:
+                            if (!string.IsNullOrEmpty(m_Texture2D.m_StreamData?.path))
+                                assetItem.FullSize = asset.byteSize + m_Texture2D.m_StreamData.size;
+                            assetItem.Text = m_Texture2D.m_Name;
+                            exportable = true;
+                            break;
+                        case AudioClip m_AudioClip:
+                            if (!string.IsNullOrEmpty(m_AudioClip.m_Source))
+                                assetItem.FullSize = asset.byteSize + m_AudioClip.m_Size;
+                            assetItem.Text = m_AudioClip.m_Name;
+                            exportable = true;
+                            break;
+                        case VideoClip m_VideoClip:
+                            if (!string.IsNullOrEmpty(m_VideoClip.m_OriginalPath))
+                                assetItem.FullSize = asset.byteSize + (long)m_VideoClip.m_ExternalResources.m_Size;
+                            assetItem.Text = m_VideoClip.m_Name;
+                            exportable = true;
+                            break;
+                        case Shader m_Shader:
+                            assetItem.Text = m_Shader.m_ParsedForm?.m_Name ?? m_Shader.m_Name;
+                            exportable = true;
+                            break;
+                        case Mesh _:
+                        case TextAsset _:
+                        case AnimationClip _:
+                        case Font _:
+                        case MovieTexture _:
+                        case Sprite _:
+                            assetItem.Text = ((NamedObject)asset).m_Name;
+                            exportable = true;
+                            break;
+                        case Animator m_Animator:
+                            if (m_Animator.m_GameObject.TryGet(out var gameObject))
+                            {
+                                assetItem.Text = gameObject.m_Name;
+                            }
+                            exportable = true;
+                            break;
+                        case MonoBehaviour m_MonoBehaviour:
+                            if (m_MonoBehaviour.m_Name == "" && m_MonoBehaviour.m_Script.TryGet(out var m_Script))
+                            {
+                                assetItem.Text = m_Script.m_ClassName;
+                            }
+                            else
+                            {
+                                assetItem.Text = m_MonoBehaviour.m_Name;
+                            }
+                            exportable = true;
+                            break;
+                        case PlayerSettings m_PlayerSettings:
+                            productName = m_PlayerSettings.productName;
+                            break;
+                        case AssetBundle m_AssetBundle:
+                            foreach (var m_Container in m_AssetBundle.m_Container)
+                            {
+                                var preloadIndex = m_Container.Value.preloadIndex;
+                                var preloadSize = m_Container.Value.preloadSize;
+                                var preloadEnd = preloadIndex + preloadSize;
+                                for (int k = preloadIndex; k < preloadEnd; k++)
+                                {
+                                    containers.Add((m_AssetBundle.m_PreloadTable[k], m_Container.Key));
+                                }
+                            }
+                            assetItem.Text = m_AssetBundle.m_Name;
+                            break;
+                        case ResourceManager m_ResourceManager:
+                            foreach (var m_Container in m_ResourceManager.m_Container)
+                            {
+                                containers.Add((m_Container.Value, m_Container.Key));
+                            }
+                            break;
+                        case NamedObject m_NamedObject:
+                            assetItem.Text = m_NamedObject.m_Name;
+                            break;
+                    }
+                    if (assetItem.Text == "")
+                    {
+                        assetItem.Text = assetItem.TypeString + assetItem.UniqueID;
+                    }
+                    if (/*Properties.Settings.Default.displayAll ||*/ exportable)
+                    {
+                        exportableAssets.Add(assetItem);
+                    }
+                    Progress.Report(++i, objectCount);
+                }
+            }
+            foreach ((var pptr, var container) in containers)
+            {
+                if (pptr.TryGet(out var obj))
+                {
+                    objectAssetItemDic[obj].Container = container;
+                }
+            }
+            containers.Clear();
+        }
+
+        public static void ExportAssets(string savePath, List<AssetItem> toExportAssets)
+        {
+            int exportedCount = 0;
+            foreach (var asset in toExportAssets)
+            {
+                string exportPath;
+                if (!string.IsNullOrEmpty(asset.Container))
+                {
+                    exportPath = Path.Combine(savePath, Path.GetDirectoryName(asset.Container));
+                }
+                else
+                {
+                    exportPath = savePath;
+                }
+                exportPath += Path.DirectorySeparatorChar;
+                if (ExportConvertFile(asset, exportPath))
+                {
+                    exportedCount++;
+                }
+            }
+            Console.WriteLine($"Finished exporting {exportedCount} assets.");
+        }
+    }
+}
